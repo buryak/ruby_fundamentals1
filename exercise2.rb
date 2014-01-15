6020b54e5254d54d4924cf0f20dc0d2e87a50840
yuri <yuri@yuris-MacBook-Pro.local>
2014-01-15 12:57:26 -0500

    Exercise 2 update

---
+++ exercise2.rb
@@ -0,0 +1,4 @@
+puts 55*0.15 # total tip on 55 dollar meal
+puts "string" + 5.to_s  # adding string and integer together using conversion
+puts "45628 multiplied by 7839 is #{45628*7839}" # string interpolation hashtagyolo
+puts (true && false) || (false && true) || !(false && false) # boolean logic
